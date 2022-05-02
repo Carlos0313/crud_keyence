d406788dd203d2c89bb08bc7a53aa323254b9d9a
Carlos Najera <carnave.cnv@gmail.com>
2022-05-01 19:51:28 -0500

    conexion con mongodb

--- db.conexion.js
+++ db.conexion.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose'); 
 
 //Conexion con la base de datos 
-const DATABASE = 'mongodb://localhost/keyence_prueba';
-
+//const DATABASE = 'mongodb://localhost/keyence_prueba';
+const DATABASE = 'mongodb+srv://carlos:Passw0rd@keyence.2u7jg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const conexionDB = async () =>{
     try{
        const DB = await mongoose.connect(DATABASE)
