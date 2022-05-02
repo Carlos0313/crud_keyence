4098bd3bbdc7fcb2b33ce3d6b9b306af524f9c84
Carlos Najera <carnave.cnv@gmail.com>
2022-05-01 20:30:55 -0500

    bugfix listen serve

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const { PORT=3000, LOCAL_ADDRESS='0.0.0.0' } = process.env
 
 //Inicializa el servidor en el puerto 3500
 app.listen(PORT, LOCAL_ADDRESS, () => {
-    const address = app.address();
-    console.log(`servidor corriendo en http://localhost:${PORT} address: ${address} `);
+    console.log(`servidor corriendo en http://localhost:${PORT}`);
 });
